4b62a92e871f49aba7c3643bdf0d937988a3b2f8
Lea <39883637+Roboolet@users.noreply.github.com>
2025-05-20 15:40:46 +0200

    Fixed IngredientReceiver and added keys for ingredients

--- Assets/Scripts/Receivers/IngredientReceiver.cs
+++ Assets/Scripts/Receivers/IngredientReceiver.cs
@@ -1,29 +1,39 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class IngredientReceiver : ArduinoMessageReceiver
 {
     [SerializeField] private IngredientGame game;
     [SerializeField] private IngredientTagPair[] pairs;
-    private Dictionary<int, Ingredient> dict = new Dictionary<int, Ingredient>();
-    
+    private Dictionary<string, Ingredient> dict = new Dictionary<string, Ingredient>();
+
+    private void Awake()
+    {
+        messageTypeFilter = MessageType.NFC;
+
+        for (int i = 0; i < pairs.Length; i++)
+        {
+            dict.Add(pairs[i].id.Trim().Normalize(), pairs[i].ingredient);
+        }
+    }
+
     protected override void ProcessStringData(string value)
     {
-        int num = int.Parse(value.Substring(0, 2));
-        
-        if (dict.TryGetValue(num, out Ingredient ing))
+        if (dict.TryGetValue(value.Trim().Normalize(), out Ingredient ing))
         {
             game.EnterIngredient(ing);
             Debug.Log("Added ingredient "+ing);
         }
-        Debug.LogWarning("No matching ingredient for id "+num);
+        Debug.LogWarning("No matching ingredient for id "+value);
     }
 
+    [System.Serializable]
     public struct IngredientTagPair
     {
-        public int id;
-        public Ingredient Ingredient;
+        public string id;
+        public Ingredient ingredient;
     }
 }
 
